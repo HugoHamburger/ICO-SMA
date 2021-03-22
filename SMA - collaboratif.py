b7852868d806a0ae13bfc6db1e1cdd3f6fbd85cf
heez77 <74363374+heez77@users.noreply.github.com>
2021-03-22 15:19:39 +0100

    Update SMA - collaboratif.py
    + pool_agent

--- SMA - collaboratif.py
+++ SMA - collaboratif.py
@@ -42,8 +42,64 @@ class rs_agent(Agent):
         
         
 class pool_agent(Agent):
+    def __init__(self, id, model, nb_solutions,pool_radius):
+        super().__init__(id,model)
+        self.nb_solutions = nb_solutions
+        self.pool = []
+        self.pr = pool_radius
+        
+    def distance(self,solution1,solution2):
+        s1 = []
+        s2=[]
+        r = 0
+        for x in solution1:
+            for i in range(1,len(x)):
+                s1.append((x[i-1],x[i]))
+        for x in solution2:
+            for i in range(1,len(x)):
+                s2.append((x[i-1],x[i]))
+        for x in s1 :
+            if not x in s2:
+                r += 1
+        return 1-r/self.pr if r<self.pr else 0
     
-    def __init__(self):
+    
+    def eval_function(self):
+        pool = self.pool
+        g = 0
+        for i in range(len(pool)-1) :
+            for y in pool[i+1:]:
+                g+= distance(y,pool[i])
+                
+                
+    def solution(self):
+        for a in self.model.schedule.agents:
+            if isinstance(a,tab_agent) or isinstance(a,rs_agent) or isinstance(a,gen_agent):
+                solution = a.solution
+                res = -1
+                if len(self.pool)< nb_solutions :
+                    phi = 0
+                    for x in self.pool:
+                        phi += self.distance(x,solution)
+                    if phi == 0:
+                        self.pool.append(solution)
+                        break
+                else :
+                    pool = self.pool
+                    g = self.eval_function(pool)
+                    for i in range(pool):
+                        pool_test = pool.copy()
+                        pool_test[i] = solution
+                        g_bis = self.eval_function(pool_test)
+                        if g_bis < g :
+                            g = g_bis
+                            res = i
+                            
+                    if res != -1 :
+                        self.pool[i]=solution
+                        
+    def step(self):
+        self.solution()
         
 
         
