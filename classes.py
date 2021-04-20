d657df81888d6ca119ebb43b78c55d9c6cae6209
Crazyp0 <81258382+Crazyp0@users.noreply.github.com>
2021-04-20 12:49:53 +0200

    Update classes.py

--- classes.py
+++ classes.py
@@ -39,21 +39,27 @@ class TruckTab:
         self.cost = 0
         self.time = 0
             
-   
     def calculate_cost(self, data,listOfClients):
         demande = 0
+        weight_K = 10000
+        weight_q = 10000
+        weight_t = 3
+        weight_d = 13
+        weight_c = 10        
         for m in self.P:
             demande += listOfClients[m].quantity
         self.remaining_quantity-=demande
         if(self.remaining_quantity<0):
-            self.cost+=self.remaining_quantity*(-1)
+            self.cost+=(self.remaining_quantity*(-1))*weight_q
         for i in range (len(self.P)-1):
-            self.cost += distanceTab (self.P[i],self.P[i+1],listOfClients)
+            self.cost += (distanceTab(self.P[i],self.P[i+1],listOfClients)*weight_c)
             self.time += data[self.P[i]][self.P[i+1]]
             if self.time <= listOfClients[self.P[i+1]].start:
+                self.cost += ((listOfClients[self.P[i+1]].start - self.time)*weight_t)
                 self.time = listOfClients[self.P[i+1]].start
             if self.time >= listOfClients[self.P[i+1]].stop:
-                self.cost += self.time - listOfClients[self.P[i+1]].stop
-        return self.cost
+                self.cost += ((self.time - listOfClients[self.P[i+1]].stop)*weight_d)
+        self.cost += weight_K
+        return (self.cost)
 def distanceTab(C1, C2, listOfClients):
         return(np.sqrt((listOfClients[C1].y-listOfClients[C2].y)**2 + (listOfClients[C1].x - listOfClients[C2].x)**2))
